40be521452c8a546f06e4a186b9e9872c41362f4
Almagribii <brucadalm@gmail.com>
2025-09-11 22:48:35 +0700

    add navbar on the homepage

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,11 +1,12 @@
 import { Footer } from "../../components/layout/footer";
-
+import { Navbar } from "../../components/layout/navbar";
 const Homepage = () => {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl">Halo Dunia!</h1>
       <p>Ini adalah komponen App yang valid.</p>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
