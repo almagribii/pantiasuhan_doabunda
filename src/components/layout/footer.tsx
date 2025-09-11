bf3f2439b3cd073ca7c9b2fa606d7b786c2aa9dd
Almagribii <brucadalm@gmail.com>
2025-09-11 19:01:27 +0700

    add footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom";
 
 export function Footer() {
-    return (
-        
-    )
+  return (
+    <footer className="border-t">
+      <div className="text-muted-foreground container mx-auto flex flex-col items-center justify-between px-4 py-6 text-sm md:flex-row md:px-8">
+        <p className="">&copy; 2024 Doa Bunda. All rights reserved.</p>
+        <div className="mt-4 flex gap-4 md:mt-0">
+          <Link to="/privacy" className="hover:text-primary transition-colors hover:underline">
+            Privacy Policy
+          </Link>
+          <Link to="/terms" className="hover:text-primary transition-colors hover:underline">
+            Terms of Service
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,8 +1,11 @@
+import { Footer } from "../../components/layout/footer";
+
 const Homepage = () => {
   return (
     <div>
       <h1 className="text-3xl">Halo Dunia!</h1>
       <p>Ini adalah komponen App yang valid.</p>
+      <Footer/>
     </div>
   );
 };
