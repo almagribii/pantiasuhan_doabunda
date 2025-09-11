cc5f12fd4060ef03041874d055665abb3e12e16d
Almagribii <brucadalm@gmail.com>
2025-09-11 18:02:24 +0700

    add logo

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Halo Dunia!</h1>
+      <p>Ini adalah komponen App yang valid.</p>
+    </div>
+  );
+};
+
+export { Homepage };
