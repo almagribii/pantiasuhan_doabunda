6d7da16580a108d539f41bd6944b019957701735
Almagribii <brucadalm@gmail.com>
2025-09-12 22:45:50 +0700

    add article current with api GNews

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Homepage } from "./pages/homepage/Homepage";
+import Article from "./pages/article/Article";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/artikel" element={<Article />} />
     </Routes>
   );
 }
