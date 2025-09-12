c3c98dfef2222c1c4361a0906be4f4621b1508b6
Almagribii <brucadalm@gmail.com>
2025-09-12 18:55:20 +0700

    apply orphanage and tabbed

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,13 +1,16 @@
 import { Footer } from "../../components/layout/footer";
 import { Navbar } from "../../components/layout/navbar";
 import ImageSlider from "./components/ImageSlider";
+import OrphanageInfoSection from "./components/OrphanageInfoSection";
+import TabbedArticleSection from "./components/TabbedArticleSection";
 const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <div className="pt-16 pb-12">
+      <div className="pt-16 ">
         <ImageSlider />
       </div>
+      <OrphanageInfoSection />
       <Footer />
     </div>
   );
