a6a31c9aefbc69c52f3a624fa2c0d6b5c3c75db7
Almagribii <brucadalm@gmail.com>
2025-09-12 21:23:09 +0700

    determine padding on homepage

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -3,15 +3,17 @@ import { Navbar } from "../../components/layout/navbar";
 import ImageSlider from "./components/ImageSlider";
 import OrphanageInfoSection from "./components/OrphanageInfoSection";
 import TabbedArticleSection from "./components/TabbedArticleSection";
+import LocationSection from "./components/LocationSection";
 const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <div className="pt-16 ">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 mt-16">
         <ImageSlider />
+        <OrphanageInfoSection />
+        <TabbedArticleSection />
+        <LocationSection />
       </div>
-      <OrphanageInfoSection />
-      <Footer />
     </div>
   );
 };
